8898cbfc49f2e2b9bfd02314f235895e48218d13
AsusIbarra <carlos.ih.developer@gmail.com>
2025-09-01 08:03:11 -0600

    Cambios de estabilidad (Revisar botón de eliminar partido)

--- snfcofcBlzrwb.Web.Client/Program.cs
+++ snfcofcBlzrwb.Web.Client/Program.cs
@@ -3,9 +3,11 @@ using snfcofcBlzrwb.Shared.Services.Implementations;
 using snfcofcBlzrwb.Shared.Services.Interfaces;
 using snfcofcBlzrwb.Shared.Services.Local;
 using snfcofcBlzrwb.Shared.Services.Remote;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped<IPlayerService, PlayerService>();
 //builder.Services.AddSingleton<IPlayerService, PlayerLocalService>();
 //builder.Services.AddSingleton<IPlayerService, PlayerRemoteService>();
 await builder.Build().RunAsync();
+builder.Services.AddSyncfusionBlazor();
